03fc11b72807828782f74a66e71464af3c360e13
Karen Gharibyan <karen.gharibyan3@gmail.com>
2025-01-28 03:04:16 +0400

    fix

--- src/common/utils/index.ts
+++ src/common/utils/index.ts
@@ -2,7 +2,7 @@ export const formatCardNumber = (value: string) => {
   const regex = /^(\d{0,4})(\d{0,4})(\d{0,4})(\d{0,4})$/g;
   const onlyNumbers = value.replace(/[^\d]/g, "");
 
-  return onlyNumbers.replace(regex, (regex, $1, $2, $3, $4) =>
+  return onlyNumbers.replace(regex, (_regex, $1, $2, $3, $4) =>
     [$1, $2, $3, $4].filter((group) => !!group).join(" ")
   );
 };
