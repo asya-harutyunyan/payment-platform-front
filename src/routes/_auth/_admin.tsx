048c4412a42e0bb672babf5ebf9f2128e8f5baf8
Karen Gharibyan <karen.gharibyan3@gmail.com>
2025-01-27 20:21:47 +0400

    added laravel echo

--- src/routes/_auth/_admin.tsx
+++ src/routes/_auth/_admin.tsx
@@ -1,4 +1,27 @@
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import Echo from "laravel-echo";
+import { useEffect } from "react";
+
+const AdminLayout = () => {
+  useEffect(() => {
+    // window.Pusher = Pusher;
+
+    window.Echo = new Echo({
+      broadcaster: "reverb",
+      key: import.meta.env.VITE_REVERB_APP_KEY,
+      wsHost: import.meta.env.VITE_REVERB_HOST,
+      wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+      wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+      forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+      enabledTransports: ["ws", "wss"],
+    });
+  }, []);
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
 
 export const Route = createFileRoute("/_auth/_admin")({
   beforeLoad: async () => {
@@ -9,9 +32,5 @@ export const Route = createFileRoute("/_auth/_admin")({
       });
     }
   },
-  component: () => (
-    <>
-      <Outlet />
-    </>
-  ),
+  component: AdminLayout,
 });
--- src/routes/_auth/_user.tsx
+++ src/routes/_auth/_user.tsx
@@ -1,4 +1,57 @@
+import { useAuth } from "@/context/auth.context";
 import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+import { useEffect } from "react";
+
+window.Pusher = Pusher;
+
+const UserLayout = () => {
+  const { user } = useAuth();
+  useEffect(() => {
+    if (user) {
+      window.Echo = new Echo({
+        broadcaster: "reverb",
+        key: import.meta.env.VITE_REVERB_APP_KEY,
+        wsHost: import.meta.env.VITE_REVERB_HOST,
+        wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+        wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+        forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? "https") === "https",
+        authorizer: (channel, options) => {
+          return {
+            authorize: (socketId, callback) => {
+              fetch(import.meta.env.VITE_BASE_API_URL + "/broadcasting/auth", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                  Authorization:
+                    "Bearer " + localStorage.getItem("accessToken"),
+                },
+                body: JSON.stringify({
+                  socket_id: socketId,
+                  channel_name: channel.name,
+                }),
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  callback(false, data);
+                })
+                .catch((error) => {
+                  callback(true, error);
+                });
+            },
+          };
+        },
+        enabledTransports: ["ws", "wss"],
+      });
+    }
+  }, []);
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
 
 export const Route = createFileRoute("/_auth/_user")({
   beforeLoad: () => {
@@ -9,9 +62,5 @@ export const Route = createFileRoute("/_auth/_user")({
       });
     }
   },
-  component: () => (
-    <>
-      <Outlet />
-    </>
-  ),
+  component: UserLayout,
 });
