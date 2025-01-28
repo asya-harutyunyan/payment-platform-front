a378ad338c45a01954e3dd1c0ced38a4d5353d64
Karen Gharibyan <kgharibyan@aionbiosystems.com>
2025-01-28 15:23:14 +0400

    .

---
+++ src/components/atoms/logo/index.tsx
@@ -0,0 +1,10 @@
+import logo from "@/assets/images/logo.png";
+import { Box } from "@mui/material";
+
+export const Logo = () => {
+  return (
+    <Box sx={{ width: "50px", height: "50px" }}>
+      <img src={logo} style={{ width: "100%", height: "100%" }} />
+    </Box>
+  );
+};
