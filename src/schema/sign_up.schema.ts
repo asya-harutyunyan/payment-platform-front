ceb35b087b8845985e45ad1cb333193901e665b1
Karen Gharibyan <kgharibyan@aionbiosystems.com>
2025-02-05 13:57:35 +0400

    .

--- src/schema/sign_up.schema.ts
+++ src/schema/sign_up.schema.ts
@@ -4,7 +4,7 @@ import { password_regex } from "./password.regex";
 export const auth_schema = z.object({
   name: z.string().min(3).max(15),
   surname: z.string().min(3).max(15),
-  email: z.string().email().max(20),
+  email: z.string().email().max(50),
   password: z.string().min(6).regex(password_regex),
   password_confirmation: z.string().min(6),
   checkbox: z.boolean(),
