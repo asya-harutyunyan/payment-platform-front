780a1c91055cd066b163ae399c1e889916202eaf
Karen Gharibyan <kgharibyan@aionbiosystems.com>
2025-04-17 13:50:11 +0400

    .

---
+++ src/components/organisms/user-orders/_services/useUserOrder.tsx
@@ -0,0 +1,232 @@
+import Button from "@/components/atoms/button";
+import { IColumn } from "@/components/molecules/table";
+import { getStatusColor } from "@/components/utils/status-color";
+import { useAuth } from "@/context/auth.context";
+import { DEPOSIT_STATUSES } from "@/enum/deposit.status.enum";
+import { useAppDispatch, useAppSelector } from "@/store";
+import {
+  confirmOrderByClientThunk,
+  getOrdersThunk,
+} from "@/store/reducers/user-info/depositSlice/thunks";
+import { Order } from "@/store/reducers/user-info/depositSlice/types";
+import { H6, P } from "@/styles/typography";
+import { useLocation, useNavigate } from "@tanstack/react-router";
+import dayjs from "dayjs";
+import { t } from "i18next";
+import { enqueueSnackbar } from "notistack";
+import { useEffect, useMemo, useState } from "react";
+import Countdown, { CountdownRenderProps } from "react-countdown";
+
+type ICountdownRendererFn = (
+  props: CountdownRenderProps,
+  id?: number
+) => React.ReactNode;
+
+const useUserOrder = () => {
+  const dispatch = useAppDispatch();
+  const [filter, setFilter] = useState<DEPOSIT_STATUSES>(DEPOSIT_STATUSES.ALL);
+  const { orders, loading, total } = useAppSelector((state) => state.deposit);
+  const [page, setPage] = useState(1);
+  const [open, setOpen] = useState<boolean>(false);
+  const [addId, setAddId] = useState<number | null>(null);
+  const navigate = useNavigate();
+  const route = useLocation();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user?.role) return;
+
+    const fetchOrders = () => {
+      dispatch(
+        getOrdersThunk({
+          page: page,
+          per_page: user.role === "admin" ? 20 : 5,
+          status_by_client: DEPOSIT_STATUSES.ALL,
+        })
+      );
+    };
+
+    fetchOrders();
+
+    const interval = setInterval(fetchOrders, 10000);
+    return () => clearInterval(interval);
+  }, [dispatch, page, user?.role]);
+
+  const onChangePage = (_event: React.ChangeEvent<unknown>, page: number) => {
+    setPage(page);
+    dispatch(
+      getOrdersThunk({
+        page,
+        per_page: user?.role === "admin" ? 20 : 5,
+      })
+    );
+  };
+
+  const handleOpen = () => setOpen(true);
+
+  const getTimer = (created_at: string, type?: "CRYPTO" | "FIAT") => {
+    const duration = type === "FIAT" ? 40 : 15;
+    return new Date(
+      dayjs()
+        .add(
+          (dayjs(created_at).add(duration, "minutes").unix() - dayjs().unix()) *
+            1000,
+          "milliseconds"
+        )
+        .format()
+    );
+  };
+
+  const countDownrenderer: ICountdownRendererFn = (
+    { completed, formatted },
+    id
+  ) => {
+    if (completed) {
+      return <H6 color="primary.main">Не оплачен</H6>;
+    }
+
+    return (
+      <Button
+        variant="contained"
+        sx={{ fontSize: "0.7rem", width: "140px" }}
+        onClick={() => {
+          handleOpen();
+          if (id) {
+            setAddId(id);
+          }
+        }}
+        text={`Подтвердить - ${formatted.minutes}:${formatted.seconds}`}
+      />
+    );
+  };
+
+  const columns = useMemo<IColumn<Order>[]>(
+    () => [
+      {
+        column: "amount",
+        currency: "wallet_deposit.order_currency",
+        valueKey: "amount",
+      },
+      {
+        column: "order_status_user",
+        renderComponent: (row: Order) => {
+          return row.status_by_client === "pending" ? (
+            <P sx={{ width: "120px" }}>
+              <Countdown
+                date={getTimer(row.created_at, "FIAT")}
+                renderer={(props) => countDownrenderer(props, row.id)}
+              />
+            </P>
+          ) : (
+            <span
+              style={{
+                display: "flex",
+                alignItems: "center",
+                color: getStatusColor(row.status_by_client ?? "-"),
+                fontWeight: 400,
+                textTransform: "capitalize",
+              }}
+            >
+              {row.status_by_client && t(row.status_by_client)}
+            </span>
+          );
+        },
+      },
+      {
+        column: "id",
+        valueKey: "transaction_id",
+      },
+      {
+        column: "card_number",
+        valueKey: "wallet_deposit.payment_method.card_number",
+      },
+      {
+        column: "left_amount",
+        currency: "wallet_deposit.order_currency",
+        valueKey: "wallet_deposit.processing_amount",
+      },
+      {
+        column: "key",
+        renderComponent: (row: Order) => (
+          <Button
+            variant="outlined"
+            text={t("see_more")}
+            sx={{ width: "130px" }}
+            onClick={() => handleSingleOrder(row.id)}
+          />
+        ),
+      },
+    ],
+    [user]
+  );
+
+  const handleFilterChange = (
+    _: React.SyntheticEvent,
+    filter: DEPOSIT_STATUSES
+  ) => {
+    setFilter(filter);
+    dispatch(
+      getOrdersThunk({
+        page,
+        per_page: user?.role === "admin" ? 20 : 5,
+        status_by_client: filter,
+      })
+    );
+  };
+
+  const handleConfirm = (num?: number) => {
+    if (!num) return;
+
+    dispatch(confirmOrderByClientThunk(num))
+      .unwrap()
+      .then((data) => {
+        if (data.is_final) {
+          setOpen(true);
+        }
+        enqueueSnackbar(t("confirm_order_success"), {
+          variant: "success",
+          anchorOrigin: { vertical: "top", horizontal: "right" },
+        });
+        dispatch(
+          getOrdersThunk({
+            page,
+            per_page: user?.role === "client" ? 5 : 20,
+          })
+        );
+      })
+      .catch(() => {
+        enqueueSnackbar(t("something_went_wrong"), {
+          variant: "error",
+          anchorOrigin: { vertical: "top", horizontal: "right" },
+        });
+      });
+  };
+
+  const handleSingleOrder = (row?: number) => {
+    if (route.pathname === "/orders" && row) {
+      navigate({ to: `/orders/${row}` });
+    }
+  };
+
+  return {
+    filter,
+    setFilter,
+    orders,
+    loading,
+    total,
+    page,
+    setPage,
+    open,
+    setOpen,
+    addId,
+    setAddId,
+    onChangePage,
+    handleOpen,
+    getTimer,
+    handleConfirm,
+    columns,
+    handleFilterChange,
+  };
+};
+
+export default useUserOrder;
--- src/components/organisms/user-orders/user-orders.tsx
+++ src/components/organisms/user-orders/user-orders.tsx
@@ -4,275 +4,31 @@ import Button from "@/components/atoms/button";
 import { CircularIndeterminate } from "@/components/atoms/loader";
 import { BasicModal } from "@/components/atoms/modal";
 import { PaginationOutlined } from "@/components/atoms/pagination";
-import DynamicTable, { IColumn } from "@/components/molecules/table";
+import DynamicTable from "@/components/molecules/table";
 import TaskHeader from "@/components/molecules/title";
-import { getStatusColor } from "@/components/utils/status-color";
-import { useAuth } from "@/context/auth.context";
 import { DEPOSIT_STATUSES } from "@/enum/deposit.status.enum";
-import { useAppDispatch, useAppSelector } from "@/store";
-import {
-  confirmOrderByClientThunk,
-  getOrdersThunk,
-} from "@/store/reducers/user-info/depositSlice/thunks";
-import { Order } from "@/store/reducers/user-info/depositSlice/types";
-import { H3, H6, P } from "@/styles/typography";
+import { H3, P } from "@/styles/typography";
 import { Box, Tab, Tabs } from "@mui/material";
-import { useLocation, useNavigate } from "@tanstack/react-router";
-import dayjs from "dayjs";
 import { t } from "i18next";
-import { enqueueSnackbar } from "notistack";
-import { FC, useEffect, useMemo, useState } from "react";
-import Countdown, { CountdownRenderProps } from "react-countdown";
+import { FC } from "react";
 import { EmptyComponent } from "../empty-component";
-
-type ICountdownRendererFn = (
-  props: CountdownRenderProps,
-  id?: number
-) => React.ReactNode;
+import useUserOrder from "./_services/useUserOrder";
 
 export const UserOrdersComponent: FC = () => {
-  const dispatch = useAppDispatch();
-  const [filter, setFilter] = useState<DEPOSIT_STATUSES>(DEPOSIT_STATUSES.ALL);
-  const { orders, loading, total } = useAppSelector((state) => state.deposit);
-  const [page, setPage] = useState(1);
-  const [open, setOpen] = useState<boolean>(false);
-  const [addId, setAddId] = useState<number | null>(null);
-
-  const { user } = useAuth();
-  useEffect(() => {
-    if (!user?.role) return;
-
-    const fetchOrders = () => {
-      dispatch(
-        getOrdersThunk({
-          page: page,
-          per_page: user.role === "admin" ? 20 : 5,
-          status_by_client: DEPOSIT_STATUSES.ALL,
-        })
-      );
-    };
-
-    fetchOrders();
-
-    const interval = setInterval(fetchOrders, 10000);
-
-    return () => clearInterval(interval);
-  }, [dispatch, page, user?.role]);
-
-  const onChangePage = (_event: React.ChangeEvent<unknown>, page: number) => {
-    setPage?.(page);
-    if (user?.role === "admin") {
-      dispatch(getOrdersThunk({ page: page, per_page: 20 }));
-    } else {
-      dispatch(getOrdersThunk({ page: page, per_page: 5 }));
-    }
-  };
-  const handleOpen = () => setOpen(true);
-  const getTimer = (created_at: string, type?: "CRYPTO" | "FIAT") => {
-    if (type) {
-      if (type === "CRYPTO") {
-        return new Date(
-          dayjs()
-            .add(
-              (dayjs(created_at).add(15, "minutes").unix() - dayjs().unix()) *
-                1000,
-              "milliseconds"
-            )
-            .format()
-        );
-      }
-
-      return new Date(
-        dayjs()
-          .add(
-            (dayjs(created_at).add(40, "minutes").unix() - dayjs().unix()) *
-              1000,
-            "milliseconds"
-          )
-          .format()
-      );
-    }
-    return new Date(
-      dayjs()
-        .add(
-          (dayjs(created_at).add(15, "minutes").unix() - dayjs().unix()) * 1000,
-          "milliseconds"
-        )
-        .format()
-    );
-  };
-  const countDownrenderer: ICountdownRendererFn = (
-    { completed, formatted },
-    id?: number
-  ) => {
-    if (completed) {
-      return <H6 color="primary.main">Не оплачен</H6>;
-    } else {
-      return (
-        <Button
-          variant={"contained"}
-          sx={{ fontSize: "0.7rem", width: "140px" }}
-          onClick={() => {
-            handleOpen();
-            if (id) {
-              setAddId(id);
-            }
-          }}
-          text={`Подтвердить- ${formatted.minutes}:
-          ${formatted.seconds}`}
-        ></Button>
-      );
-    }
-  };
-  const columns = useMemo<IColumn<Order>[]>(
-    () => [
-      {
-        column: "amount",
-        currency: "wallet_deposit.order_currency",
-        valueKey: "amount",
-      },
-      {
-        column: "order_status_user",
-        renderComponent: (row: Order) => {
-          return row.status_by_client === "pending" ? (
-            <>
-              {" "}
-              <P
-                sx={{
-                  width: "120px",
-                }}
-              >
-                <Countdown
-                  date={getTimer(row.created_at as string, "FIAT")}
-                  renderer={(props) => {
-                    return countDownrenderer(props, row.id);
-                  }}
-                />
-              </P>
-            </>
-          ) : (
-            <span
-              style={{
-                display: "flex",
-                alignItems: "center",
-                color: getStatusColor(row.status_by_client ?? "-"),
-                fontWeight: 400,
-                textTransform: "capitalize",
-              }}
-            >
-              {row.status_by_client && t(row.status_by_client)}
-            </span>
-          );
-        },
-      },
-      {
-        column: "id",
-        valueKey: "transaction_id",
-      },
-      {
-        column: "card_number",
-        valueKey: "wallet_deposit.payment_method.card_number",
-      },
-      {
-        column: "left_amount",
-        currency: "wallet_deposit.order_currency",
-        valueKey: "wallet_deposit.processing_amount",
-      },
-      {
-        column: "key",
-        renderComponent: (row: Order) => {
-          return (
-            <Button
-              variant={"outlined"}
-              text={t("see_more")}
-              sx={{ width: "130px" }}
-              onClick={() => handleSingleOrder?.(row.id)}
-            />
-          );
-        },
-      },
-    ],
-    []
-  );
-
-  const handleConfirm = (num?: number) => {
-    if (num) {
-      dispatch(confirmOrderByClientThunk(num))
-        .unwrap()
-        .then((data) => {
-          if (data.is_final) {
-            setOpen(true);
-          }
-          enqueueSnackbar(t("confirm_order_success"), {
-            variant: "success",
-            anchorOrigin: { vertical: "top", horizontal: "right" },
-          });
-          if (user?.role === "client") {
-            dispatch(getOrdersThunk({ page, per_page: 5 }));
-          } else {
-            dispatch(getOrdersThunk({ page, per_page: 20 }));
-          }
-        })
-        .catch(() => {
-          enqueueSnackbar(t("something_went_wrong"), {
-            variant: "error",
-            anchorOrigin: { vertical: "top", horizontal: "right" },
-          });
-        });
-    }
-  };
-  const navigate = useNavigate();
-  const route = useLocation();
-
-  const handleSingleOrder = (row?: number) => {
-    if (route.pathname === "/orders") {
-      navigate({ to: `/orders/${row}` });
-    }
-  };
-
-  // const refetch = () => {
-  //   if (user?.role === "admin") {
-  //     dispatch(
-  //       getOrdersThunk({
-  //         page: page,
-  //         per_page: 20,
-  //         // status_by_client: DEPOSIT_STATUSES.ALL,
-  //       })
-  //     );
-  //   } else {
-  //     dispatch(
-  //       getOrdersThunk({
-  //         page: page,
-  //         per_page: 5,
-  //         // status_by_client: DEPOSIT_STATUSES.ALL,
-  //       })
-  //     );
-  //   }
-  // };
-
-  const handleFilterChange = (
-    _: React.SyntheticEvent,
-    filter: DEPOSIT_STATUSES
-  ) => {
-    setFilter(filter);
-    if (user?.role === "admin") {
-      dispatch(
-        getOrdersThunk({
-          page: page,
-          per_page: 20,
-          status_by_client: filter,
-        })
-      );
-    } else {
-      dispatch(
-        getOrdersThunk({
-          page: page,
-          per_page: 5,
-          status_by_client: filter,
-        })
-      );
-    }
-  };
+  const {
+    filter,
+    orders,
+    loading,
+    total,
+    page,
+    open,
+    setOpen,
+    addId,
+    onChangePage,
+    handleFilterChange,
+    handleConfirm,
+    columns,
+  } = useUserOrder();
 
   return (
     <Box sx={{ width: "100%" }}>
