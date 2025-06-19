5d16f72a203c487a7169a9b1db32f5cd968efdef
v-babayan <vladislavbabayan@gmail.com>
2025-06-19 21:43:29 +0400

    feat: add helper for file download

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,3 +2,16 @@ export function addFivePercent(value: number): number {
   const updatedPrice = value * 1.05;
   return Math.ceil(updatedPrice / 50) * 50;
 }
+
+export async function downloadFile(url: string, filename: string) {
+  const response = await fetch(url);
+  const blob = await response.blob();
+
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+  URL.revokeObjectURL(a.href);
+}
