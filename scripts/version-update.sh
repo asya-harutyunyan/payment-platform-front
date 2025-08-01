32ef7d3e0c414e6352006d4d32354d9cf3c47e7a
Karen Gharibyan <kgharibyan@aionbiosystems.com>
2025-08-01 14:48:25 +0400

    changed script

--- scripts/version-update.sh
+++ scripts/version-update.sh
@@ -1,11 +1,10 @@
-
 FILE="info.json"
 
 # Check if file exists
 if [ -f "$FILE" ]; then
   # Read current version
   VERSION=$(jq '.version' "$FILE")
-  if [ -z "$VERSION" ] || ! [[ "$VERSION" =~ ^[0-9]+$ ]]; then
+  if [ -z "2" ] || ! [[ "$VERSION" =~ ^[0-9]+$ ]]; then
     echo "Invalid version in file. Resetting to 0."
     VERSION=0
   fi
@@ -16,9 +15,10 @@ fi
 
 # Increment version
 NEW_VERSION=$((VERSION + 1))
+DATA_RELEASE=$(date '+31.07');
 
 # Write updated version to file
-echo "{\"version\": $NEW_VERSION}" > "$FILE"
+echo "{\"version\": $NEW_VERSION, \"data_release\": \"$DATA_RELEASE\" }" > "$FILE"
 
 # Confirm
 echo "Updated version to $NEW_VERSION in $FILE"
