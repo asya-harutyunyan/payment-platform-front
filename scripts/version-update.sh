d0ac1dec5962c51b5fb51d9d2636047de9a604d6
karen_gharibyan <karen.gharibyan3@gmail.com>
2025-08-01 10:49:59 +0000

    hotfix-script

--- scripts/version-update.sh
+++ scripts/version-update.sh
@@ -4,7 +4,7 @@ FILE="info.json"
 if [ -f "$FILE" ]; then
   # Read current version
   VERSION=$(jq '.version' "$FILE")
-  if [ -z "2" ] || ! [[ "$VERSION" =~ ^[0-9]+$ ]]; then
+  if [ -z "$VERSION" ] || ! [[ "$VERSION" =~ ^[0-9]+$ ]]; then
     echo "Invalid version in file. Resetting to 0."
     VERSION=0
   fi
@@ -15,7 +15,7 @@ fi
 
 # Increment version
 NEW_VERSION=$((VERSION + 1))
-DATA_RELEASE=$(date '+31.07');
+DATA_RELEASE=$(date '+%d-%m-%H.%M');
 
 # Write updated version to file
 echo "{\"version\": $NEW_VERSION, \"data_release\": \"$DATA_RELEASE\" }" > "$FILE"
