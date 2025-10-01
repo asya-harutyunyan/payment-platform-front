1816e8a214d64ac3a26114dc6e9d0113f0179956
Asya Harutyunyan <asya.harutyunyan010@gmail.com>
2025-10-01 15:11:50 +0400

    chat new iframe added

--- src/components/organisms/chat-page/chat.tsx
+++ src/components/organisms/chat-page/chat.tsx
@@ -4,9 +4,9 @@ const ChatUI = () => {
   return (
     <Box sx={{ backgroundColor: "white", width: "100%", height: "100%" }}>
       <iframe
-        src="https://payment-platform.rocketsystems.net/chat/channels/town-square?token=bjnac79bof88pezps4efkzq4fh"
-        width="100%"
-        height="600px"
+        src="https://chat.payhub.email/payhub/channels/town-square?token=pepgsgpwitfuzeeqrro78856mc"
+        style={{ width: "100%", height: "600px", border: "none" }}
+        allow="clipboard-write; fullscreen"
       />
     </Box>
   );
